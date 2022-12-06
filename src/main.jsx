f2d6ca0eb353408673906aad342612daa60d4b74
Alexander Celis <alexcelis019@gmail.com>
2022-12-05 21:35:54 -0500

    added bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 /* components */
 import App from './components/App'
 /* styles */
+import 'bootstrap/dist/css/bootstrap.css'
 import './styles/index.css'
 
 const root = createRoot(document.getElementById('root'))
