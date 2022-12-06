1b48476c5eef8d1c96a140f70455a6e29b1bf6fb
Alexander Celis <alexcelis019@gmail.com>
2022-12-05 20:55:09 -0500

    reseted project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+/* components */
+import App from './components/App'
+/* styles */
+import './styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
